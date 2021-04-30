4ff2b2ce4b84917c0d69362fca1af0243d28dd45
HARSHITHA <harshithachava@gmail.com>
2021-04-30 19:54:05 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -6,3 +6,19 @@ def test_show_elements():
     file = open('studentList.txt', 'r')
     assert print("Student List\n\n" + str(file.read())) == main.show_elements()
     file.close()
+
+
+def test_search():
+    file = open('studentList.txt', 'r')
+    output = main.search(str(102), file)
+    assert print("Record Found\nRoll.No: 102\nName:Sunny") == output
+    file.close()
+
+
+def test_search_element():
+    file = open('studentList.txt', 'r')
+    output = main.search(str(101), file)
+    assert print("Record Found\nRoll.No: 101\nName:Harshitha") == output
+    file.close()
+
+
